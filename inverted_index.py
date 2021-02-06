2656f981f0344cdbfc3c72b7bc5d8cb13bbb78aa
xenant <56933139+xenant@users.noreply.github.com>
2021-02-05 16:37:58 -0800

    Update inverted_index.py

--- inverted_index.py
+++ inverted_index.py
@@ -1,1 +1,14 @@
+"""
+Create an inverted index for the corpus with data structures designed by you.
+•Tokens:  all alphanumeric sequences in the dataset.
+
+•Stop words:  do not use stopping while indexing, i.e.  use all words, eventhe frequently occurring ones.•Stemming: use stemming for better textual matches.  
+               Suggestion: Porterstemming, but it is up to you to choose.
+
+
+•Important text:  text in bold (b, strong), in headings (h1, h2, h3), andin  titles  should  be  treated  as  more  important  than  the  in  other  places.
+Verify which are the relevant HTML tags to select the important words.
+
+"""
+
 
