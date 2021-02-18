3cf8b596c46399ddc7ec80b43abea75dc0f1a7ce
xenant <56933139+xenant@users.noreply.github.com>
2021-02-17 19:39:34 -0800

    Update Milestone_2.py

--- Milestone_2.py
+++ Milestone_2.py
@@ -4,7 +4,28 @@ from collections import defaultdict # when we find doc/term frequency.
 from nltk.tokenize import RegexpTokenizer # use this to find tokens that are alphanumeric, but also numbers with decimals (but not next to letters) 
 from  nltk.stem import PorterStemmer
 from bs4 import BeautifulSoup
+import math 
+
+ranking = defaultdict(float)
+
+def query_search(user_input):
+    queries = user_input.split()
+
+    for query in queries:
+        for document in index_dict:
+            # get term freq from document 
+            tf_idf = 1 + math.log( term_frequency) # TF part only
+            tf_idf += math.log(len(index_dict)/ len (index_dict[query])
+
+
+    if document in ranking:
+        ranking[document] += tf_idf
+    else:
+        ranking[document] = tf_idf
+
+
+    sorted_ranks = sorted(ranking.items(), key = lambda i : (-i[1])
 
 
 
-def 
+
