3cdee4a49949b1c88299fd7035c1a8e10205abe4
Lillian Won <lillian.won@live.com>
2021-03-03 14:18:22 -0800

    Added html form to get user queries

--- webgui.py
+++ webgui.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template
+
+
 app = Flask(__name__)
 
-@app.route('/')
+# GET - A GET msg is sent and server returns data
+# POST -  Send HTML form data to server. Data received POST 
+@app.route('/', methods=['GET', 'POST'])
 def search_page():
-    return 'search page' 
+    # create search form
+    if request.method == 'POST':
+        queries = request.form['query']
+
+
+    return render_template("search_page.html")
+
+
+
+# This is so you can just do py -3 webgui.py
+if __name__ == '__main__':
+    app.run(debug = True)
